b2f8f182374f2ab329fec1e59b94dc78951fc077
Ikeoluwa Atulomah <adesinaikeoluwa4@gmail.com>
2024-03-19 23:54:45 -0700

    Sprint 7 module 4 done

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from './App'
+import index from '../i18n/index.json'
 
 describe('Module 4 Project Tests', () => {
   describe('English Language', () => {
@@ -12,33 +13,201 @@ describe('Module 4 Project Tests', () => {
     */
     test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
       render(<App lang="en" />)
-      expect(screen.getByText("Create an Account")).toBeVisible()
+      expect(screen.getByText(index.en.TEXT_HEADING_CREATE_ACCOUNT)).toBeVisible()
     })
+
+    test(`TEXT_FAV_LANG_JS is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_FAV_LANG_JS)).toBeVisible()
+    })
+    test(`TEXT_FAV_LANG_RUST is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_FAV_LANG_RUST)).toBeVisible()
+    })
+    test(`TEXT_OPT_FAV_FOOD_1 is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_OPT_FAV_FOOD_1)).toBeVisible()
+    })
+
+    test(`TEXT_OPT_FAV_FOOD_2 is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_OPT_FAV_FOOD_2)).toBeVisible()
+    })
+
+    test(`TEXT_OPT_FAV_FOOD_3 is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_OPT_FAV_FOOD_3)).toBeVisible()
+    })
+    test(`TEXT_OPT_FAV_FOOD_4 is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_OPT_FAV_FOOD_4)).toBeVisible()
+    })
+    test(`TEXT_SUBMIT is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_SUBMIT)).toBeVisible()
+    })
+
+    test(`TEXT_FAV_LANG is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.TEXT_FAV_LANG)).toBeVisible()
+    })
+
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.LABEL_USERNAME)).toBeVisible()
+    })
+
+    test(`LABEL_FAV_FOOD is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.LABEL_FAV_FOOD)).toBeVisible()
+    })
+
+    test(`LABEL_ACCEPT_TERMS is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByText(index.en.LABEL_ACCEPT_TERMS)).toBeVisible()
+    })
+
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="en" />)
+      expect(screen.getByPlaceholderText(index.en.PLACEHOLDER_USERNAME)).toBeVisible()
+    })
+
   })
   describe('Spanish Language', () => {
     /*
       👉 TASK 3
 
       This is done after making the UI multilingual.
+      
     */
-  })
-  describe('getEntriesByKeyPrefix', () => {
-    test('can extract the correct data', () => {
-    /*
-      👉 TASK 4 part 2
-
-      Implement the function `getEntriesByKeyPrefix` below
-      and then come back here and write a few tests
-      to ensure it works as expected.
-
-      Although it should be noted that commonly,
-      the tests are written _before_ implementing
-      the function being tested.
-    */
+    test(`TEXT_HEADING_CREATE_ACCOUNT is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_HEADING_CREATE_ACCOUNT)).toBeVisible()
     })
+    test(`TEXT_FAV_LANG_JS is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_FAV_LANG_JS)).toBeVisible()
+    })
+
+    test(`TEXT_FAV_LANG_RUST is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_FAV_LANG_RUST)).toBeVisible()
+    })
+
+    test(`TEXT_OPT_FAV_FOOD_1 is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_OPT_FAV_FOOD_1)).toBeVisible()
+    })
+
+    test(`TEXT_OPT_FAV_FOOD_2 is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_OPT_FAV_FOOD_2)).toBeVisible()
+    })
+
+    test(`TEXT_OPT_FAV_FOOD_3 is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_OPT_FAV_FOOD_3)).toBeVisible()
+    })
+
+    test(`TEXT_OPT_FAV_FOOD_4 is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_OPT_FAV_FOOD_4)).toBeVisible()
+    })
+
+    test(`TEXT_SUBMIT is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_SUBMIT)).toBeVisible()
+    })
+
+    test(`TEXT_FAV_LANG is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.TEXT_FAV_LANG)).toBeVisible()
+    })
+
+    test(`LABEL_USERNAME is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.LABEL_USERNAME)).toBeVisible()
+    })
+
+    test(`LABEL_FAV_FOOD is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.LABEL_FAV_FOOD)).toBeVisible()
+    })
+
+    test(`LABEL_ACCEPT_TERMS is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByText(index.esp.LABEL_ACCEPT_TERMS)).toBeVisible()
+    })
+
+    test(`PLACEHOLDER_USERNAME is visible`, () => {
+      render(<App lang="esp" />)
+      expect(screen.getByPlaceholderText(index.esp.PLACEHOLDER_USERNAME)).toBeVisible()
+    })
+
+
   })
 })
+
+
+describe('getEntriesByKeyPrefix', () => {
+  test('can extract the correct data', () => {
+
+
+    const obj = {
+      abc_1: "data_abc_1",
+      abc_2: "data_abc_2",
+      xyz_1: "data_xyz_1",
+      abc_3: "data_abc_3",
+    }
+    const keyPrefix = "abc";
+    const expected =
+      [
+        ["abc_1", "data_abc_1"],
+        ["abc_2", "data_abc_2"],
+        ["abc_3", "data_abc_3"],
+      ];
+    const result = getEntriesByKeyPrefix(obj, keyPrefix);
+    expect(result).toEqual(expected);
+  });
+
+  test("should return an empty array if no properties with given prefixes are found", () => {
+
+    const obj = {
+      abc_1: "data_abc_1",
+      abc_2: "data_abc_2",
+      xyz_1: "data_xyz_1",
+      abc_3: "data_abc_3",
+    }
+    const keyPrefix = "wxy"
+    const result = getEntriesByKeyPrefix(obj, keyPrefix);
+    expect(result).toEqual([]);
+  });
+
+  /*
+    👉 TASK 4 part 2
+ 
+    Implement the function `getEntriesByKeyPrefix` below
+    and then come back here and write a few tests
+    to ensure it works as expected.
+ 
+    Although it should be noted that commonly,
+    the tests are written _before_ implementing
+    the function being tested.
+  */
+})
+
 function getEntriesByKeyPrefix(obj, keyPrefix) {
+  const result = [];
+  for (const key in obj) {
+    if (key.startsWith(keyPrefix + "_")) {
+      result.push([key, obj[key]]);
+    }
+  }
+  return result;
+
+
+
+
   /*
     👉 TASK 4 part 1
 
